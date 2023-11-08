1d362933164e0b58f4236e0143c483b4f002bc77
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-11-08 15:10:08 -0500

    welcome message change

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Welcome = () => {
     return (
-        <h1 className='flex'>Why are we doing this? to make sure we remember what we spent 20k on!</h1>
+        <div>
+            <h1 className='flex'>Why are we doing this? to make sure we remember what we spent 20k on!</h1>
+                <p>never forget!</p>
+
+        </div>
     )
 }
 
